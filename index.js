5b24c7a5ef099cd2707d77bd96140ea6316052bf
Alan <alanrd772@gmail.com>
2021-02-17 16:35:45 +0400

    fixed grammar

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const messages = JSON.parse(fs.readFileSync('result.json', 'utf8')).messages;
 
 if (!messages)
-    console.log("Messages JSON file not found. Is it named result.json, in the root folder of the program and exported as JSON via https://github.com/Tyrrrz/DiscordChatExporter")
+    console.log("Messages JSON file not found. Is it named result.json, in the root folder of the program and exported as JSON via https://github.com/Tyrrrz/DiscordChatExporter?")
 
 let result = {
     messages: { },
